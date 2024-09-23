c97517bfd2a1469ba28896c856b446dcea293409
rogermix69 <rogermix69@hotmail.com>
2024-09-23 15:04:37 -0300

    entrega do desafio

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-console.log("oi");
-console.log("Rogerio Cardoso");
+let heroi = prompt("Digite o nome de seu Heroi");
+let xp = prompt("Qual o nivel de xp do seu heroi");
+if (xp <= 1000) {
+  alert(" O heroi " + heroi + " tem de " + xp + " XP o nivel é Prata");
+} else if (xp >= 1001 && xp < 2000) {
+  alert(" O heroi " + heroi, " tem de " + xp + " XP o nivel é Bronze");
+} else if (xp >= 2001 && xp < 3000) {
+  alert(" O heroi " + heroi + " tem de " + xp + " XP o nivel é Ouro");
+} else if (xp >= 3001 && xp < 4000) {
+  alert(" O heroi " + heroi + " tem de " + xp + " xp o nivel é Platina");
+} else {
+  alert(" O heroi " + heroi + " tem de " + xp + " XP o nivel é Acendente");
+}
